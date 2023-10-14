d231d19d3f7a88fba0b09059f9bd25d91e548857
rafaelalma <rafaelalma92@gmail.com>
2023-10-14 13:54:26 +0200

    install: react instead of preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
-import preact from "@astrojs/preact";
+import { defineConfig } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rafaelalma.netlify.app/',
-  integrations: [preact(), tailwind()]
-});
+  integrations: [react(), tailwind()],
+})
